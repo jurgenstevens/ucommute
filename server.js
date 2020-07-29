667a13ebe529778207774cd0e6bcbca7a675d443
Jurgen <jurgen.stevens@yahoo.com>
2020-07-29 12:47:54 -0500

    Starting from the beginning the same way as the mumblr blog site.

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
+// S1: In terminal type 'npm init -y' to create package.json file//
+// S2: In terminal type 'npm install'
+// S3: In terminal type 'npm install express mongoose cors"
 const express = require("express");
+const mongoose = require("mongoose");
