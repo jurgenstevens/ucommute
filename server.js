fe73bc9af750464b84f9bdae6c817808f1382b2c
Jurgen <jurgen.stevens@yahoo.com>
2020-07-29 13:20:58 -0500

    Server is now connected and MongoDB connection is now established.

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 // S1: In terminal type 'npm init -y' to create package.json file//
 // S2: In terminal type 'npm install'
-// S3: In terminal type 'npm install express mongoose cors"
+// S3: In terminal type 'npm install express mongoose cors dotenv"
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.once("open", () =>
+  console.log("MongoDB connection established successfully")
+);
+
+app.listen(port, () => console.log(`The app is running on ${port}`));
