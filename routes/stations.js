a5c0ebcad0f6c0708fb08d2da372f9d6ce2e4178
Jurgen <jurgen.stevens@yahoo.com>
2020-07-29 22:02:14 -0500

    Imported the stations schema for the stations routes.

--- routes/stations.js
+++ routes/stations.js
@@ -1,3 +1,7 @@
 const express = require("express");
+// S10: this will help us route in our application
 const router = express.Router();
-const Articles = require("../models/stations");
+// S11: import the schema from the models folder
+const Stations = require("../models/stations");
+
+router.get("/", (req, res) => {});
