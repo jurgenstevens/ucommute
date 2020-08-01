eb6d2870d1aaf8eb46db2720aee270e92e984d89
Jurgen <jurgen.stevens@yahoo.com>
2020-08-01 00:00:43 -0500

    Finished the promise int he get request with then and catch.

--- routes/stations.js
+++ routes/stations.js
@@ -8,5 +8,7 @@ const Stations = require("../models/stations");
 // S12: GET request to get ALL of the posts to DISPLAY
 router.get("/", (req, res) => {
   // we're going to find the post from the schema then create the promise to return our posts in JSON format
-  Stations.find();
+  Stations.find()
+  .then((post) => res.json(post))
+  .catch(err) => res.status(400).res.json(`Error: ${err}`)
 });
