393526b8028dacae20dcab7f0a5ca9bb3a55f92d
Jurgen <jurgen.stevens@yahoo.com>
2020-07-31 23:59:06 -0500

    Set the get route up with the find method for the imported Stations model.

--- routes/stations.js
+++ routes/stations.js
@@ -4,4 +4,9 @@ const router = express.Router();
 // S11: import the schema from the models folder
 const Stations = require("../models/stations");
 
-router.get("/", (req, res) => {});
+// this will be our first route
+// S12: GET request to get ALL of the posts to DISPLAY
+router.get("/", (req, res) => {
+  // we're going to find the post from the schema then create the promise to return our posts in JSON format
+  Stations.find();
+});
