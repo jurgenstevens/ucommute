7c2bf26ffea9ecc68c76f886974040f7ae19eb33
Jurgen <jurgen.stevens@yahoo.com>
2020-08-11 22:53:08 -0500

    Imported the Trips schema from the models folder.

--- routes/trips.js
+++ routes/trips.js
@@ -1,3 +1,5 @@
 const express = require("express");
 //  S10: This will help us route in our Trips
 const router = express.Router();
+//  S11: Import the schema from the models folder
+const Trips = require("../models/trips");
